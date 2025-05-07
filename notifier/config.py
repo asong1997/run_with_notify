c629893692cbdef6e1cb00e29acf1210ffdf4f36
王振松 <wangzhensong@lakala.com>
2025-05-07 15:28:39 +0800

    更新项目

--- notifier/config.py
+++ notifier/config.py
@@ -1,3 +1,3 @@
-default_from_email = "13360057872@163.com"
-default_password = "YGba3bz8uT6jGSZT"
-default_to_email = "473826249@qq.com"
+default_from_email = "1336xxx2@163.com"
+default_password = "YGbaxxxjGSZT"
+default_to_email = "4738xxxx@qq.com"
